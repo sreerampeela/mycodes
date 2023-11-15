0bb46506d8f63b2835b32761686f4c760911c0b5
sreerampeela <59723039+sreerampeela@users.noreply.github.com>
2023-11-15 12:40:01 +0530

    Update dataAnalysis_lamp_qpcr.R
    Added code for comparing ct of lamp + and lamp - samples

--- dataAnalysis_lamp_qpcr.R
+++ dataAnalysis_lamp_qpcr.R
@@ -6,18 +6,6 @@ shapiro.test(df_ct$qpcr)
 dfCSF = subset(df_ct, df_ct$group == "csf")
 dfCAP = subset(df_ct, df_ct$group == "Blood")
 
-
-### Menigitis ###
-mean(dfCSF$qlamp)
-mean(dfCSF$qpcr)
-qLAMP2 = (dfCSF$qlamp - mean(dfCSF$qlamp))/sd(dfCSF$qlamp)
-qPCR2 = (dfCSF$qpcr -mean(dfCSF$qpcr))/sd(dfCSF$qpcr)
-shapiro.test(qPCR)
-shapiro.test(qLAMP)
-var.test(qLAMP, qPCR)
-t.test(qLAMP,qPCR, var.equal = F)
-cor.test(qLAMP,qPCR)
-
 ### CAP ##
 mean(dfCAP$qlamp)
 sd(dfCAP$qlamp)
@@ -30,5 +18,28 @@ shapiro.test(qPCR)
 shapiro.test(qLAMP)
 wilcox.test(qLAMP, qPCR) # data not normally distributed
 cor.test(qLAMP,qPCR)
-hist(qPCR)
-hist(qLAMP)
+
+
+### Menigitis ###
+mean(dfCSF$qlamp)
+mean(dfCSF$qpcr)
+qLAMP2 = (dfCSF$qlamp - mean(dfCSF$qlamp))/sd(dfCSF$qlamp)
+qPCR2 = (dfCSF$qpcr -mean(dfCSF$qpcr))/sd(dfCSF$qpcr)
+shapiro.test(qPCR2)
+shapiro.test(qLAMP2)
+var.test(qLAMP2, qPCR2)
+t.test(qLAMP2,qPCR2, var.equal = F)
+cor.test(qLAMP2,qPCR2)
+
+boxplot(qLAMP, qPCR, qLAMP2, qPCR2, names = c("qLAMP-CAP", "qPCR-CAP","qLAMP-MEN","qPCR-MEN"), ylab = " Normalised CT/CLAMP value")
+
+## comparing mean Ct of lamp+ve and lamp-ve pcr
+df_pcr <- read.csv("lamp_p_vs_n.csv", header = T)
+lampPos = subset(df_pcr$ct, df_pcr$lamp == "lamp+")
+shapiro.test(lampPos)
+lampNeg = subset(df_pcr$ct, df_pcr$lamp == "lamp -ve")
+shapiro.test(lampNeg)
+## not normally distributed
+boxplot(lampPos, lampNeg, names = c("qLAMP +ve", "qLAMP -ve"), ylab = "CT value")
+wilcox.test(lampPos, lampNeg)
+
